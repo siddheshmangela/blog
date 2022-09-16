4bef22006ae90fde451a7028f54b8cc259cf1a26
Siddhesh Mangela <sidthecool007@gmail.com>
2022-09-16 12:19:44 +0530

    fixes added

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog by Siddhesh Mangela';
+export const SITE_TITLE = "Siddhesh Mangela's Blog";
 export const SITE_DESCRIPTION = 'Welcome to my blog!';
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,12 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
+console.log(import.meta.glob('./*.mdx'));
+
 export const get = () =>
   rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob('./*.md'),
+    items: import.meta.glob('./*.mdx'),
   });
