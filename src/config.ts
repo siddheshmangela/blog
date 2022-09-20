4a9ca009e8e9ce4701c8ddf2177ccf40a9560e79
Siddhesh Mangela <sidthecool007@gmail.com>
2022-09-20 13:54:26 +0530

    description added

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Siddhesh Mangela's Blog";
-export const SITE_DESCRIPTION = 'Welcome to my blog!';
+export const SITE_DESCRIPTION = 'Stories from India and beyond!';
